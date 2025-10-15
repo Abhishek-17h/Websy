2400707fb16e240d5d268d620e6ee4588d3578a8
Abhishek-17H <abhishekhegaje1702@gmail.com>
2025-10-15 12:10:11 +0530

    added inngest for background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import prisma from '@/lib/db';
+import { useTRPC } from '@/trpc/client';
 
 export default function Home(){
+  const trpc= useTRPC();
+  trpc.hello.queryOptions({text:'from tRPC'});
    const users = prisma.user.findMany();
   return (
     <div>
