fa9f71dbe8e9547f155f373d4656058c1b31d676
Abhishek-17H <abhishekhegaje1702@gmail.com>
2025-10-17 09:43:51 +0530

    verified working of inngest and trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,36 @@
-"use client"
+"use client";
+import { useTRPC } from "@/trpc/client";
+import { toast } from "sonner";
+import { useMutation } from "@tanstack/react-query";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { use, useState } from "react";
 
-import { useTRPC } from '@/trpc/client';
-
-export default function Home(){
+const Page = () => {
+  const [value, setValue] = useState("");
   const trpc = useTRPC();
-  trpc.hello.queryOptions({text:"world"});
-   
-  return (
-    <div>
-      "hello world"
-    </div>
-  )
-}
+  const invoke = useMutation(
+    trpc.invoke.mutationOptions({
+      onSuccess: () => {
+        toast.success("background job started");
+      },
+    })
+  );
 
+  return (
+    <div className="p-4 max-w-7xl mx-auto flex justify-center">
+      <Input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        className="border p-2 rounded mr-2"
+      />
+      <Button
+        disabled={invoke.isPending}
+        onClick={() => invoke.mutate({ value: value })}
+      >
+        Invoked background job
+      </Button>
+    </div>
+  );
+};
+export default Page;
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,17 +1,23 @@
+import { inngest } from '@/inngest/client';
 import { z } from 'zod';
 import { baseProcedure, createTRPCRouter } from '../init';
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
+  invoke: baseProcedure
     .input(
       z.object({
-        text: z.string(),
+        value: z.string(),
       }),
     )
-    .query((opts) => {
-      return {
-        greeting: `hello ${opts.input.text}`,
-      };
-    }),
+    .mutation(async({input})=>{
+      await inngest.send({
+        name:"test/hello.world",
+        data:{
+          value:input.value
+        }
+      })
+      return {ok:"success"};
+    })
+    
 });
-// export type definition of API
+
 export type AppRouter = typeof appRouter;
