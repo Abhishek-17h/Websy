72ce2c6974049eedb035d710e97a2df8ef60baca
Abhishek-17H <abhishekhegaje1702@gmail.com>
2025-10-18 09:29:52 +0530

    tested openai api working

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,19 @@
 import { inngest } from "./client";
+import { openai, createAgent } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "5s");
-    return { message: `Hello ${event.data.value}!` };
-  },
+  async ({ event }) => {
+    const summerizer = createAgent({
+      name: "summerizer",
+      system: "You are an expert summerizer.  You summerize in 2 words.",
+      model: openai({ model: "gpt-4o" }),
+    });
+
+    const { output } = await summerizer.run(
+      `Summarize the following text in 2 words: ${event.data.value}`
+    );
+    return { output };
+  }
 );
